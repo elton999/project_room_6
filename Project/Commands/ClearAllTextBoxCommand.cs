86ec0b098b6c311861750f214fddde2cb05bcea6
Elton Silva <eltoncarsilva73@gmail.com>
2023-11-19 00:05:30 -0300

    command to show text box

---
+++ Project/Commands/ClearAllTextBoxCommand.cs
@@ -0,0 +1,20 @@
+﻿using Project.Interfaces;
+using Project.UI;
+using UmbrellaToolsKit;
+
+namespace Project.Commands
+{
+    public class ClearAllTextBoxCommand : ICommand
+    {
+        private Scene _scene;
+
+        public ClearAllTextBoxCommand(Scene scene) => _scene = scene;
+
+        public void Execute() 
+        {
+            foreach (var gameobject in _scene.UI)
+                if (gameobject is TextBox)
+                    gameobject.RemoveFromScene = true;
+        }
+    }
+}
---
+++ Project/Commands/ShowTextBoxCommand.cs
@@ -0,0 +1,43 @@
+﻿using Project.Interfaces;
+using UmbrellaToolsKit;
+using Microsoft.Xna.Framework;
+using Project.UI;
+
+namespace Project.Commands
+{
+    public class ShowTextBoxCommand : ICommand
+    {
+        private string _text;
+        private Scene _scene;
+
+        private TextBox _textBox;
+
+        public ShowTextBoxCommand(string text, Scene scene)
+        {
+            _scene = scene;
+
+            _textBox = new TextBox();
+            _textBox.Scene = scene;
+            SetText(text);
+        }
+
+        public void Execute()
+        {
+            SetText(_text);
+            _textBox.Start();
+            _scene.UI.Add(_textBox);
+            _textBox.ContentSize = GetStringSize();
+            _textBox.Position = _scene.Sizes.ToVector2() / 2f - (_textBox.ContentSize + Vector2.One * 16) / 2f;
+            _textBox.Position = _textBox.Position.ToPoint().ToVector2();
+        }
+
+        public void SetText(string text)
+        {
+            _text = text;
+            _textBox.Text = text;
+        }
+
+        private Vector2 GetStringSize() =>  _textBox.Font.MeasureString(_text);
+
+    }
+}
