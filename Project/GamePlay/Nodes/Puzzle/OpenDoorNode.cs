b0784dcf033eb921a2a593ed25c61fec2ce36eb2
Elton Silva <eltoncarsilva73@gmail.com>
2024-02-12 13:31:05 -0300

    added supporte IDoor interface on OpenDoorNode class

--- Project/GamePlay/Nodes/Puzzle/OpenDoorNode.cs
+++ Project/GamePlay/Nodes/Puzzle/OpenDoorNode.cs
@@ -1,19 +1,20 @@
 ﻿using Microsoft.Xna.Framework;
+using Project.Interfaces;
 using UmbrellaToolsKit.BehaviorTrees;
 
 namespace Project.GamePlay.Nodes.Puzzle
 {
     public class OpenDoorNode : Node
     {
-        private PuzzleButtons _puzzleButtons;
+        private IDoor _doorController;
 
-        public OpenDoorNode(PuzzleButtons puzzleButtons) => _puzzleButtons = puzzleButtons;
+        public OpenDoorNode(IDoor doorController) => _doorController = doorController;
 
         public override NodeStatus Tick(GameTime gameTime)
         {
             if (!(bool)GetData("wasOpened"))
             {
-                var door = _puzzleButtons.Door;
+                var door = _doorController.Door;
                 door.Node = new SequenceNode();
                 door.Node.Add(new Entities.DoorNodes.OpenDoorNode(door));
 
