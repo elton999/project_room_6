8948f9b1abd7c8585545ebabf179b89951e072d9
Elton Silva <eltoncarsilva73@gmail.com>
2024-02-24 14:30:40 -0300

    add file path class

---
+++ Project/FilePath.cs
@@ -0,0 +1,16 @@
+﻿namespace Project
+{
+    public class FilePath
+    {
+        // GENERAL
+        public const string SPRITE_PATH = "Sprites/Tilemap/tilemap";
+        public const string SPRITE_ATLAS_PATH = "Sprites/Tilemap/atlas";
+        // FONTS
+        public const string BASIC_FONT_PATH = "Kenney Mini";
+        // PLAYER
+        public const string PLAYER_SPRITE_PATH = "Sprites/Player/player";
+        public const string PLAYER_ANIMATION_PATH = "Sprites/Player/player";
+        // CHARACTERS
+        public const string CHARACTERS_SPRITE_PATH = "Sprites/characters";
+    }
+}
