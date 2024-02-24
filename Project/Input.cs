7b5ffdec0ae0e8aaa23b9682b9c635c77609cb29
Elton Silva <eltoncarsilva73@gmail.com>
2024-02-24 15:02:46 -0300

    add input class names

---
+++ Project/Input.cs
@@ -0,0 +1,12 @@
+﻿namespace Project
+{
+    public class Input
+    {
+        public const string INTERACT = "interact";
+        public const string EXIT = "exit";
+        public const string UP = "up";
+        public const string DOWN = "down";
+        public const string LEFT = "left";
+        public const string RIGHT = "right";
+    }
+}
