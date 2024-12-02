2a6573fa2b4b269e8a5515c8ca25e21789e81566
Elton Silva <eltoncarsilva73@gmail.com>
2024-12-01 22:47:09 -0300

    fix file path references

--- Project/FilePath.cs
+++ Project/FilePath.cs
@@ -9,7 +9,7 @@
         public const string BASIC_FONT_PATH = "Kenney Mini";
         // PLAYER
         public const string PLAYER_SPRITE_PATH = "Sprites/Player/player";
-        public const string PLAYER_ANIMATION_PATH = "Sprites/Player/player";
+        public const string PLAYER_ANIMATION_PATH = "Sprites/Player/player_animation";
         // CHARACTERS
         public const string CHARACTERS_SPRITE_PATH = "Sprites/characters";
     }
